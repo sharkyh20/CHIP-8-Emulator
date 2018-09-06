7ede54847aaf2c93ae95bdacf945633235796fde
sharkyh20 <dooger1@live.com.au>
2018-09-07 08:32:11 +1000

    Beginnings of main loop structure

---
+++ src/CHIP8_EMU/chip8.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "stdafx.h"
+#include <string>
+
+class Chip8 {
+public:
+    void initialise() {
+        // Initialise registers and memory once
+    }
+    void loadGame(std::string gameName) {
+        
+    }
+
+    void emulateCycle() {
+        // Fetch opcode
+        // Decode opcode
+        // Execute opcode
+
+        // Update timers
+    }
+};
